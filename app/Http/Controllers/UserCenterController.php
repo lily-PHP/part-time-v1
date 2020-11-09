695111e94a44e47352ca80da23614438a54bf2c4
lily <lily_phper@163.com>
2020-11-10 00:39:51 +0800

    注册

--- app/Http/Controllers/UserCenterController.php
+++ app/Http/Controllers/UserCenterController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Mews\Captcha\Captcha;
+use Illuminate\Support\Facades\Validator;
+use DB;
 
 class UserCenterController extends Controller
 {
@@ -40,34 +42,75 @@ class UserCenterController extends Controller
 
 
     /**
-     * 校验验证码
+     * 用户注册
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function userRegister(Request $request)
     {
         $return = config('return.select');
-        $key = $request -> input('sid') ?? '';
-        $cap = $request -> input('code') ?? '';
-        if (!$key || !$cap) {
+        $all = $request -> all();
+
+        $message = [
+            'username.required' => '用户名：邮箱必填',
+            'password.required' => '密码必填',
+            'name.required'     => '昵称必填',
+            'sid.required'      => 'UUID缺失',
+            'code.required'     => '验证码必填',
+        ];
+        $validator = Validator::make($all, [
+            'username'      => 'required',
+            'password'      => 'required',
+            'name'          => 'required',
+            'sid'           => 'required',
+            'code'          => 'required'
+        ], $message);
+
+        if ($validator -> fails()) {
+            $return['msg'] = $validator -> errors() -> first();
             return response() -> json($return);
         }
 
         $redis = app('redis.connection');
         $redis -> select(6);
-        $cache_cap = $redis -> get('Captcha_'.$key);
+        $cache_cap = $redis -> get('Captcha_'.$all['sid']);
 
         if (!$cache_cap) {
             $return['msg'] = '验证码过期~';
             return response() -> json($return);
         }
 
-        if (!captcha_api_check($cap, $cache_cap)) {
-            $return['status'] = 403;
+        if (!captcha_api_check($all['code'], $cache_cap)) {
+            $return['status'] = 40002;
             $return['msg'] = '验证码错误';
+            return response() -> json($return);
+        }
+
+        //验证码正确，校验用户名是否存在
+        if (DB::table('user') -> where('u_userName', $all['username']) -> exists()) {
+            $return['status'] = 40000;
+            $return['msg'] = '用户名已被注册';
+            return response() -> json($return);
+        }
+
+        if (DB::table('user') -> where('u_nickName', $all['name']) -> exists()) {
+            $return['status'] = 40001;
+            $return['msg'] = '昵称已被注册';
+            return response() -> json($return);
+        }
+
+        $password = md5('lily_'.$all['password']);
+        $res = DB::table('user') -> insert([
+            'u_userName' => addslashes($all['username']),
+            'u_password' => $password,
+            'u_nickName' => addslashes($all['name'])
+        ]);
+
+        if (!$res) {
+            $return['msg'] = '注册失败，请刷新重试~';
         } else {
             $return['status'] = 200;
-            $return['msg'] = '验证码正确~';
+            $return['msg'] = '注册成功~';
         }
 
         return response() -> json($return);
